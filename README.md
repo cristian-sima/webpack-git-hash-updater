eecc4807859da7f779908304c509df53621d74a3
Cristian Sima <cristian.sima93@gmail.com>
2022-01-27 20:39:06 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,179 @@
+# webpack-git-hash-updater
+
+## It requires **webpack version 5**!
+
+*If you need to use webpack v4 please use this script [here](https://github.com/alleyinteractive/webpack-git-hash)*.
+
+#
+
+This is a Webpack v5 for solving problem of caching the same content served by the webpack bundle, by altering the `githash` of the js files from your HTML files. 
+
+## How to install 
+
+```
+npm install webpack-git-hash-updater
+```
+
+## Simple example
+
+Your HTML files are in the `/static/` folder
+
+```jsx
+
+const
+  fs = require("fs"),
+  colors = require("cli-color"),
+  webpack = require("webpack"),
+
+  WebpackGitHashUpdater = require("webpack-git-hash-updater"),
+
+  entries = [
+    {
+      name : "game",
+      path : "Game/entryPoint",
+    },
+    {
+      name : "console",
+      path : "Console/entryPoint",
+    },
+  ],
+
+  gitHashPlugin = new WebpackGitHashUpdater({
+    cleanup  : true,
+    callback : (hash) => entries.map(({ name }) => {
+
+      const filename = `${indexFolderPath}/${name}.html`;
+
+      return fs.readFile(filename, "utf8", (errRead, data) => {
+
+        if (errRead) {
+          return console.log(errRead);
+        }
+
+        const
+          reg = new RegExp(`src="\/static\/${name}-\\w+.js"`, "u"),
+          newData = data.replace(
+            reg,
+            `src="/static/${name}-${hash}.js"`,
+          );
+
+        return fs.writeFile(filename, newData, (errWrite) => {
+          if (errWrite) {
+            return console.log(errWrite);
+          }
+
+          WebpackGitHash5.log(
+            `File ${colors.cyanBright(filename)} updated. hash: ${colors.cyanBright(hash)}`,
+          );
+          return null;
+        });
+      });
+    }),
+  });
+
+ // ... bla bla bla other things
+  
+  webpackConfiguration = {
+    // .... other things
+    plugins: isDevelopmentMode ? [
+		gitHashPlugin,
+	] : []
+	// ... other things
+  };
+
+```
+
+## Why?
+
+Webpack already [gives](https://webpack.github.io/docs/long-term-caching.html) you the `[hash]` placeholder, which serves exactly this purpose. This plugin has a couple advantages:
+
+1. Using the Git hash in the filename allows you to very quickly pinpoint where in the commit history to look for any issues you encounter on a production site.
+1. The plugin can automatically delete old versions of the files it affects. Depending on how you manage static assets, this might be useful.
+1. You can specify the length of the hash. If you have some crazy restriction on the number of characters in your file names, maybe this will help.
+
+## How it works ?
+
+Use the placeholder `[githash]` in your config.
+
+```jsx
+var WebpackGitHash = require('webpack-git-hash-updater');
+
+module.exports = {
+	output: {
+		filename: 'bundle.[githash].js',
+		chunkFilename: '[name]-chunk.[githash].js'
+	},
+	plugins: [
+		new WebpackGitHash()
+	]
+}
+```
+
+### Options
+
+You can pass these options when you instantiate the plugin in your `plugins` array:
+
+#### `placeholder`
+Defaults to `[githash]`. Pass another string to use as the placeholder in filenames.
+
+#### `cleanup`
+Defaults to `false`. Pass `true` to delete old versions after Webpack is finished. This works by searching for files in the output directory that match the same pattern as the files that were just compiled, except for the hash. Note the the number of characters in the hash must match. For example:
+
+```jsx
+bundle.gha3k8d.js -> the newly compiled bundle
+bundle.lk8adsm.js -> would be deleted
+bundle.987aas880m.js -> would *NOT* be deleted
+```
+
+#### `callback`
+Optional callback function that runs on Webpack's `done` [step](https://webpack.github.io/docs/plugins.html#done). It receives three arguments:
+
+1. `hash` The hash used as the latest version by the plugin (_not_ Webpack's `[hash]`).
+1. `deletedFiles` Array of any filenames (without path) deleted during cleanup; this might be an empty array.
+1. `stats` Webpack [stats](https://webpack.github.io/docs/node.js-api.html#stats) object from the latest compilation
+
+#### `skipHash`
+
+Defaults to hash of most recent Git commit on the current branch. This is the unique string that will be used as the version identifier, and will be "skipped" if the plugin is set to delete old versions when Webpack is finished. See `cleanup` above.
+
+#### `hashLength`
+
+Defaults to 7 characters, which is Git's default for a "short" hash. `hashLength` can be specified _only_  when `skipHash` is not specified. If `skipHash` _is_ specified, then `hashLength` is set to the number of characters in `skipHash`.
+
+#### `outputPath`
+
+Defaults to `output.path` in your Webpack config. You can change that here though; an *absolute* path is recommended since that's what Webpack tends to use.
+
+#### `regex`
+
+If you use the `cleanup` option to delete old verions, the plugin attempts to create regular expressions to match the filenames, based on the original Webpack config. For instance:
+```
+[name]-chunk.[githash].min.js -> the config's output.chunkFilename
+abcd123 -> the latest Git hash
+/\w+-chunk\.(?!abcd123)\w{7}\.min\.js/ -> the default regex
+global-chunk.1234abc.min.js -> this would be deleted
+```
+If the default regex isn't working for you, you can specify a new `RegExp` in `regex.filename` and/or `regex.chunkFilename`. Note that there's not (yet) a way to dynamically skip the current Git hash (the `(?!abcd123)` part in the example). So if you use this option, you'll need to use the `skipHash` option also.
+
+## Post-compilation updates
+
+Here's a simple example of how to use the `callback` option to edit a `<script>` tag to load the load the latest versionof a file.
+
+```jsx
+module.exports = {
+	plugins: [
+		new WebpackGitHash({
+			cleanup: true,
+			callback: function(versionHash) {
+				var indexHtml = fs.readFileSync('./index.html', 'utf8');
+				indexHtml = indexHtml.replace(/src="\/static\/app-bundle\.\w+\.js/, 'src="/static/app-bundle.' + versionHash + '.js');
+				fs.writeFileSync('./index.html', indexHtml);
+			}
+		})
+	]
+}
+```
+
+# Acknowledgment
+
+This script was inspired by [v4 original script](https://github.com/alleyinteractive/webpack-git-hash)
---
+++ index.js
@@ -0,0 +1,248 @@
+/* eslint-enable */
+/* eslint-disable no-console */
+/* globals module */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const
+  fs = require("fs"),
+  path = require("path"),
+  childProcess = require("child_process"),
+  colors = require("cli-color");
+
+const
+  charsFromLastCommitHash = 7,
+  pluginName = "WebpackGitHash5";
+
+class WebpackGitHash5 {
+
+  /**
+  * Setup bindings and options
+  */
+  constructor (opts = {}) {
+
+    this.opts = opts;
+
+    this.doPlaceholder = this.doPlaceholder.bind(this);
+    this.cleanupFiles = this.cleanupFiles.bind(this);
+    this.loopFiles = this.loopFiles.bind(this);
+    this.deleteObsoleteFile = this.deleteObsoleteFile.bind(this);
+
+    // If not cleaning up, bind the callback directly
+    if (!this.cleanup) {
+      this.doCallback = this.doCallback.bind(this);
+    }
+
+    // Optional callback function that receives the hash and list of deleted files
+    this.callback = opts.callback || null;
+    if (typeof this.callback === "function") {
+      this.callback = this.callback.bind(this);
+    }
+
+    // Custom placeholder or default to [githash]
+    this.placeholder = opts.placeholder || "[githash]";
+
+    // Delete old versions?
+    this.cleanup = opts.cleanup || false;
+
+    // Can specify a specific hash/version
+    if (opts.skipHash) {
+      this.skipHash = opts.skipHash;
+      this.hashLength = this.skipHash.length;
+    } else {
+      // Or specify how many chars to use from the last commit hash
+      this.hashLength = opts.hashLength || charsFromLastCommitHash;
+      this.skipHash = WebpackGitHash5.getSkipHash(this.hashLength);
+    }
+
+    // Can specify output path
+    this.outputPath = opts.outputPath || null;
+
+    // Pre-specify regexes for filename and chunkFilename
+    this.regex = opts.regex || {};
+
+    // Config filled in later
+    this.updated = {};
+    this.deletedFiles = [];
+    this.stats = null;
+  }
+
+  /**
+  * Test if a file can be deleted, then delete it
+  */
+  deleteObsoleteFile (filename) {
+    if ((this.regex.filename && this.regex.filename.test(filename)) ||
+       (this.regex.chunkFilename && this.regex.chunkFilename.test(filename))) {
+      // delete synchronously so we know when loopFiles() is complete
+      fs.unlinkSync(path.join(this.outputPath, filename));
+      WebpackGitHash5.log(`Deleted ${colors.cyanBright(filename)}`);
+      this.deletedFiles.push(filename);
+    }
+  }
+
+  /**
+  * Loop through files after reading folder contents
+  */
+  loopFiles (err, contents) {
+    if (err) {
+      throw err;
+    }
+    contents.forEach(this.deleteObsoleteFile);
+    this.doCallback();
+  }
+
+  /**
+  * Callback function if one exists
+  */
+  doCallback (stats = this.stats) {
+    // Webpack stats passed directly, or stored earlier, or null
+    if (typeof this.callback === "function") {
+      this.callback(this.skipHash, this.deletedFiles, stats);
+    }
+  }
+
+  /**
+  * Delete static chunk JS files containing a hash other than the one we want to skip
+  */
+  cleanupFiles (stats) {
+    // eslint-disable-next-line
+    WebpackGitHash5.log(`Cleaning up Webpack files; skipping ${colors.cyanBright(this.placeholder)} : ${colors.cyanBright(this.skipHash)}`);
+
+    // Save Webpack stats for later
+    if (stats) {
+      this.stats = stats;
+    }
+
+    fs.readdir(this.outputPath, this.loopFiles);
+  }
+
+
+  /**
+  * Attempt to replace the placeholder string in a output string
+  */
+  doPlaceholder (key, original) {
+    const newString = original.replace(this.placeholder, this.skipHash);
+
+    if (newString === original) {
+      return false;
+    }
+    this.regex[key] = this.regex[key] || WebpackGitHash5.buildRegex(newString, this.skipHash);
+    return newString;
+  }
+
+  /**
+  * Hook into webpack plugin architecture
+  */
+  apply (compiler) {
+
+    // WebpackGitHash5.log(colors.cyanBright("Started"));
+
+    // [
+    //   `this.cleanup:${this.cleanup}`,
+    //   `this.updated.filename:${this.updated.filename}`,
+    //   `this.updated.chunkFilename:${this.updated.chunkFilename}`,
+    //   `this.outputPath:${this.outputPath}`,
+    //   "",
+    //   `compiler.options.output.filename: ${compiler.options.output.filename}`,
+    //   `compiler.options.output.chunkFilename:${compiler.options.output.chunkFilename}`,
+    //   `compiler.options.output.path:${compiler.options.output.path}`,
+
+    // ].forEach(WebpackGitHash5.log);
+
+    // Process filename and chunkFilename
+    this.updated.filename = (
+      compiler.options.output.filename
+        ? this.doPlaceholder("filename", compiler.options.output.filename)
+        : false
+    );
+
+    if (this.updated.filename) {
+      compiler.options.output.filename = this.updated.filename;
+      WebpackGitHash5.log(
+        `Changed output.filename to ${colors.cyanBright(compiler.options.output.filename)}`,
+      );
+    }
+
+    this.updated.chunkFilename = (
+      compiler.options.output.chunkFilename
+        ? this.doPlaceholder("chunkFilename", compiler.options.output.chunkFilename)
+        : false
+    );
+
+    if (this.updated.chunkFilename) {
+      compiler.options.output.chunkFilename = this.updated.chunkFilename;
+      WebpackGitHash5.log(
+        `Changed output.chunkFilename to ${colors.cyanBright(compiler.options.output.chunkFilename)}`,
+      );
+    }
+
+    if (!this.outputPath) {
+      this.outputPath = compiler.options.output.path;
+    }
+
+
+    // [
+    //   "at the end ",
+    //   `this.cleanup:${this.cleanup}`,
+    //   `this.updated.filename:${this.updated.filename}`,
+    //   `this.updated.chunkFilename:${this.updated.chunkFilename}`,
+    //   `this.outputPath:${this.outputPath}`,
+    //   "",
+    //   `compiler.options.output.filename: ${compiler.options.output.filename}`,
+    //   `compiler.options.output.chunkFilename:${compiler.options.output.chunkFilename}`,
+    //   `compiler.options.output.path:${compiler.options.output.path}`,
+
+    // ].forEach(WebpackGitHash5.log);
+
+    if (this.cleanup === true && (this.updated.filename || this.updated.chunkFilename)) {
+      compiler.hooks.done.tap(pluginName, this.cleanupFiles);
+    } else {
+      compiler.hooks.done.tap(pluginName, this.doCallback);
+    }
+
+    // WebpackGitHash5.log(colors.cyanBright("Ended"));
+  }
+
+  /**
+  * Get hash of last git commit
+  */
+  static getSkipHash (length) {
+    const skipHash = (
+      childProcess.execSync(`git rev-parse --short=${length} HEAD`, { encoding: "utf8" })
+    );
+
+    return skipHash.trim();
+  }
+
+  /**
+  * Turn processed filename into regex for later cleanup
+  */
+  static buildRegex (template, hash) {
+  // discard any prepended directories
+    const parts = template.split("/");
+
+    let
+      regex = parts.pop();
+
+    // Replace Webpack placeholders, e.g.
+    // '[name]-chunk.1234567.min.js' -> '[-_\\w]+-chunk.1234567.min.js'
+    regex = regex.replace(/\[\w+\]/giu, "[-_\\w]+");
+
+    // escape dots, e.g.
+    // '\\w+-chunk.1234567.js' -> '\\w+-chunk\\.1234567\\.js'
+    regex = regex.replace(/\./gu, "\\.");
+
+    // replace hash
+    // '\\w+-chunk\\.1234567\\.min\\.js' -> '\\w+-chunk\\.(?!1234567)\\w{7}\\.min\\.js'
+    regex = regex.replace(hash, `(?!${hash})\\w{${hash.length}}`);
+
+    // eslint-disable-next-line require-unicode-regexp
+    return new RegExp(regex);
+  }
+
+  static log (text) {
+    console.log(`<i> ${colors.greenBright("[webpack-dev-server] [webpack-git-hash]")} ${text}`);
+  }
+}
+
+
+module.exports = WebpackGitHash5;
